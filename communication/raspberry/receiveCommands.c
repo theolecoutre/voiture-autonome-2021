4e470c74610631afeecd42e6b797fe3fa4c575cb
LuizGiserman <luizgiserman@poli.ufrj.br>
2021-11-03 16:59:33 +0100

    Listening to the server

---
+++ communication/raspberry/receiveCommands.c
@@ -0,0 +1,59 @@
+#include "receiveCommands.h"
+
+void receiveCoordinates()
+{
+
+}
+
+void receiveStopVoiture()
+{
+    
+}
+
+int main ()
+{
+    int sock, sockServer;
+    struct sockaddr_in adrRBPI, adrServer;
+    int choice, length;
+    char *errorStr;
+
+    char buffer [MAX_BUFFER_LENGTH + 1];
+
+    /*establishing connection with the server*/
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    check_error("Error opening socket\n", sock, ERROR);
+
+    adrRBPI.sin_family      = AF_INET;
+    adrRBPI.sin_port        = htons(LOCAL_PORT);
+    adrRBPI.sin_addr.s_addr = inet_addr(LOCAL_IP); 
+
+    check_error("Bind Error\n", bind(sock, (const struct sockaddr *) &adrRBPI, sizeof(adrRBPI)), ERROR);
+
+    check_error("Listen Error\n", listen(sock, MAX_CONN), ERROR);
+
+    length = sizeof(adrServer);
+    check_error("Accept Error\n", sockServer = accept(sock, (struct sockaddr *) &adrServer,  &length), ERROR);
+
+    /*reading the desired command from the server*/
+    bzero(buffer, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+
+    choice = (int) strtol(buffer, &errorStr, 10);
+    
+    switch(choice)
+    {
+        case 1:
+            receiveCoordinates();
+            break;
+
+        case 2:
+            receiveStopVoiture();
+            break;
+
+        default:
+
+    }
+
+    return 0;
+}
---
+++ communication/raspberry/receiveCommands.h
@@ -0,0 +1,28 @@
+#ifndef RECEIVE_COMMANDS_H
+#define RECEIVE_COMMANDS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "../auxiliarFunctions.h"
+
+#define ERROR               -1
+#define LOCAL_PORT          3000
+#define LOCAL_IP            "127.0.0.1"
+#define REMOTE_PORT         3000
+#define REMOTE_IP           "127.0.0.1"
+#define MAX_BUFFER_LENGTH   80
+#define MAX_CONN            1
+
+void receiveCoordinates();
+void receiveStopVoiture();
+
+
+#endif
