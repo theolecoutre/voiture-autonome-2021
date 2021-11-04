f98616b13dde15f0fd2ebecd05862d7fe472055b
MrCorny812 <66158102+MrCorny812@users.noreply.github.com>
2021-11-04 11:49:48 +0100

    Update Encoders.ino

--- Firmware/Encoders.ino
+++ Firmware/Encoders.ino
@@ -32,6 +32,8 @@ void InitEncoders()
 { 
   pinMode(PORT1_NE2,INPUT_PULLUP);
   pinMode(PORT1_NE1,INPUT_PULLUP);
+  pinMode(PORT2_NE2,INPUT_PULLUP);
+  pinMode(PORT2_NE1,INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PORT1_NE2),ISR_encoder1,RISING);
 }
 
