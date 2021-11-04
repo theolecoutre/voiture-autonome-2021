3b7dfe8bb03ed636c70238becd8c3fca3a58f87b
MrCorny812 <66158102+MrCorny812@users.noreply.github.com>
2021-11-04 12:03:58 +0100

    Update Firmware.ino

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -12,8 +12,13 @@ void setup() {
  // initialization of the serial communication.
  Serial.begin(115200);
  Serial.setTimeout(10);
-
- 
+  
+ //Définition de Variables envoyé par la caméra
+  int obstacle =  0; //Lorsque la caméra détecte un obstacle 
+  int obstaclePosition = 0; //Savoir si l'obstacle se trouve à gauche ou à droite 
+  int vitesseMax = 3; // Deux mode : Zone 50 (avec 3V) et Zone 30 (avec 2V)
+  int arret = 0; // On va attendre la passage d'un piéton
+  
 }
 
 
@@ -29,8 +34,8 @@ void loop() {
 
   LogVariables();
 
-  setM1AVoltage(-3);
-  setM2AVoltage(3);
+  setM1AVoltage(-3,vitesseMax);
+  setM2AVoltage(vitesseMax);
 }
 
 void UpdateInputSignals(){
