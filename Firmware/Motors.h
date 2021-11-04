a9293373a3b947b3f2cd1d27515f7457a6577134
MrCorny812 <66158102+MrCorny812@users.noreply.github.com>
2021-11-04 11:18:57 +0100

    Update Motors.h

--- Firmware/Motors.h
+++ Firmware/Motors.h
@@ -3,6 +3,12 @@
 #define BI1 35
 #define BI2 34
 
+#define M1_PWM 12 
+#define M1_BI1 35
+#define M1_BI2 34
+
+#define M2_PWM 
+
 #define MAXPWM 190 // maximum duty cycle for the PWM is 255/MAXPWM
 #define VOLT_TO_PWM 255.0/12.0
 
