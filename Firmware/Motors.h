298ece4e0dfebbda4a33ec64844b17ec158e7dab
MrCorny812 <66158102+MrCorny812@users.noreply.github.com>
2021-11-04 11:22:17 +0100

    Update Motors.h

--- Firmware/Motors.h
+++ Firmware/Motors.h
@@ -1,13 +1,18 @@
-// PIN utilisées pour l'alimentation moteur
+// PIN utilisées pour l'alimentation moteur 1 (on garde pour le moment les variables initiales)
 #define PWM 12
 #define BI1 35
 #define BI2 34
 
+
+//PIN utilisées pour l'alimentation moteur 1 
 #define M1_PWM 12 
 #define M1_BI1 35
 #define M1_BI2 34
 
-#define M2_PWM 
+//PIN utilisées pour l'alimentation moteur 2
+#define M2_PWM 19
+#define M2_BI1 37
+#define M2_BI2 36
 
 #define MAXPWM 190 // maximum duty cycle for the PWM is 255/MAXPWM
 #define VOLT_TO_PWM 255.0/12.0
